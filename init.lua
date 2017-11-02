6cdb5293d66c100e6e120d50c9f5f2a04578d9d4
elettrico <elettrico@diciannove.net>
2017-11-02 18:39:14 -0300

    changed name materials -> bricks

--- init.lua
+++ init.lua
@@ -4,5 +4,4 @@ modname = minetest.get_current_modname();
 modpath = minetest.get_modpath(modname)
 
 -- include basic nodes files
---dofile(modpath.."/materials.lua")
-dofile(modpath.."/materials.lua")
+dofile(modpath.."/bricks.lua")
